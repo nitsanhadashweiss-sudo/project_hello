ea0a0579ec23344b6b7aee143dff11dd45645874
nitsan <nitsanhadashweiss@gmail.com>
2026-09-02 10:59:30 +0300

    o

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-print("shabat shalom")
-###
+#1
+print(" Hello Enter your  name: ")
+
+#2
+print(" Hello Enter your adress: ")
+
+
+
+"""
+name = "Name" 
+print(" Hello " , name ," How are you: " )
+"""
